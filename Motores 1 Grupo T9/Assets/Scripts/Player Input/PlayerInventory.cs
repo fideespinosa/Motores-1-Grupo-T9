04c715b5a7ee8ad7342d6286ec20238a5b2a6d5b
RooMarcos <cherryrooma@gmail.com>
2026-06-02 18:11:45 -0300

    [ADD] PC - Pasar lvl

--- Motores 1 Grupo T9/Assets/Scripts/Player Input/PlayerInventory.cs
+++ Motores 1 Grupo T9/Assets/Scripts/Player Input/PlayerInventory.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class PlayerInventory : MonoBehaviour
@@ -13,14 +14,22 @@ public class PlayerInventory : MonoBehaviour
     public int insumosCollected;
 
     [Header("Recursos necesarios para ganar")]
-    [SerializeField] private int metalNeeded = 4;
-    [SerializeField] private int combustibleNeeded = 3;
-    [SerializeField] private int insumosNeeded = 3;
+    [SerializeField] public int metalNeeded = 4;
+    [SerializeField] public int combustibleNeeded = 3;
+    [SerializeField] public int insumosNeeded = 3;
 
-    [Header("GUI")]
+    [Header("Siguiente Nivel")]
+    [SerializeField] public string nextLevel;
+
+    [Header("GUI - Dron")]
     [SerializeField] private TextMeshProUGUI textMetal;
     [SerializeField] private TextMeshProUGUI textCombustible;
     [SerializeField] private TextMeshProUGUI textInsumos;
+
+    [Header("GUI - Player")]
+    [SerializeField] private TextMeshProUGUI textPMetal;
+    [SerializeField] private TextMeshProUGUI textPCombustible;
+    [SerializeField] private TextMeshProUGUI textPInsumos;
 
     public void CollectResource(resourcesManager.ResourceType type)
     {
@@ -30,34 +39,40 @@ public class PlayerInventory : MonoBehaviour
             case resourcesManager.ResourceType.Metal:
                 metalCollected++;
                 textMetal.text = metalCollected.ToString();
+                textPMetal.text = metalCollected.ToString();
                 break;
 
             case resourcesManager.ResourceType.Combustible:
                 combustibleCollected++;
                 textCombustible.text = combustibleCollected.ToString();
+                textPCombustible.text = combustibleCollected.ToString();
                 break;
 
             case resourcesManager.ResourceType.InsumosElectronicos:
                 insumosCollected++;
                 textInsumos.text = insumosCollected.ToString();
+                textPInsumos.text = insumosCollected.ToString();
                 break;
         }
+    }
 
-        if (metalCollected >= metalNeeded &&
-            combustibleCollected >= combustibleNeeded &&
-            insumosCollected >= insumosNeeded && SceneManager.GetActiveScene().name == "Level0")
-        {
-            Debug.Log("Ganó el lvl 0");
-            SceneManager.LoadScene("Level1");
-            return;
-        }
-
+    public void CheckVictory()
+    {
+        Debug.Log("Chequea");
+        
         if (metalCollected >= metalNeeded &&
             combustibleCollected >= combustibleNeeded &&
             insumosCollected >= insumosNeeded)
         {
-            SceneManager.LoadScene("Victory");
+            SceneManager.LoadScene(nextLevel);
+            return;
         }
 
+        Debug.Log("Faltan materiales");
+
+    }
+
+    public void LevelUp()
+    {
     }
 }
