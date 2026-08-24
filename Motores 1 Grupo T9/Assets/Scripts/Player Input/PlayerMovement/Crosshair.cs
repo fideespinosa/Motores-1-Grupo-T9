5cbfd2b4e2a5ccac1b5fde9af1bf18480f365a4b
fideespinosa <fideespinosa2003@gmail.com>
2026-08-24 00:26:20 -0300

    creacion de escena principal. movimiento, crosshair dinamico e interaccion con carta

---
+++ Motores 1 Grupo T9/Assets/Scripts/Player Input/PlayerMovement/Crosshair.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Crosshair : MonoBehaviour
+{
+    [SerializeField] float rayDistance;
+    [SerializeField] LayerMask layerMask;
+    [Header("Crosshair")]
+    [SerializeField] Image crosshairImage;
+    [SerializeField] Sprite defaultSprite;
+    [SerializeField] Sprite interactSprite;
+
+    private IInteractable currentHoveredInteractable;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 origin = transform.position;
+        Vector3 direction = transform.forward;
+        if (Physics.Raycast(origin, direction, out hit, rayDistance, layerMask))
+        {
+            Debug.DrawLine(origin, hit.point, Color.red);
+            crosshairImage.sprite = interactSprite;
+
+            IInteractable interactable = null;
+            if (hit.collider.gameObject.CompareTag("Item"))
+            {
+                interactable = hit.collider.GetComponent<IInteractable>();
+            }
+
+            UpdateHover(interactable);
+
+            if (interactable != null && Input.GetMouseButtonDown(0))
+            {
+                interactable.Action();
+            }
+        }
+        else
+        {
+            crosshairImage.sprite = defaultSprite;
+            Debug.DrawRay(origin, direction * rayDistance, Color.green);
+            UpdateHover(null);
+        }
+    }
+
+    private void UpdateHover(IInteractable newInteractable)
+    {
+        if (newInteractable == currentHoveredInteractable) return;
+
+        if (currentHoveredInteractable != null)
+        {
+            currentHoveredInteractable.OnHoverExit();
+        }
+
+        if (newInteractable != null)
+        {
+            newInteractable.OnHoverEnter();
+        }
+
+        currentHoveredInteractable = newInteractable;
+    }
+}
